30f62318c889fa6c62a9450147bc4fc687d1f936
Litfa <admin@litf.com.cn>
2022-08-01 22:01:02 +0800

    feat: 总览

--- src/pages/Admin/Index.vue
+++ src/pages/Admin/Index.vue
@@ -1,8 +1,25 @@
 <script lang="ts" setup>
+import overviewApi from '@/apis/overview'
+import { ref } from 'vue'
 
+const data = ref<{
+  count: number
+  status0: number
+  status1: number
+} | null>(null)
+
+const getOverview = async () => {
+  const { data: res } = await overviewApi()
+  data.value = res.data
+}
+getOverview()
 </script>
 
-<template>今日审核</template>
+<template>
+  <div>总数:{{ data?.count }}</div>
+  <div>未验证qq：{{ data?.status0 }}</div>
+  <div>待审核{{ data?.status1 }}</div>
+</template>
 
 <style lang="less" scoped>
 </style>
