d944610ec0aa09cdca7d9ab85ffd35d26a67ec60
Litfa <admin@litf.com.cn>
2022-08-01 21:22:30 +0800

    fix: 权限验证

--- src/pages/Admin/Admin.vue
+++ src/pages/Admin/Admin.vue
@@ -1,5 +1,16 @@
 <script lang="ts" setup>
+import { useStore } from '@/config/store'
+import { useRouter } from 'vue-router'
+import { useMessage } from 'naive-ui'
 
+const store = useStore()
+const router = useRouter()
+const message = useMessage()
+
+if (!store.isLogin || store.permissions < 5) {
+  message.error('请先登录')
+  router.push('/login')
+}
 </script>
 
 <template>
