f0ca6b9a2aa1454c4a6746c3061603d12efea127
Litfa <admin@litf.com.cn>
2022-08-07 14:32:45 +0800

    fix: 未登录首页空白问题

--- src/utils/getUserInfo.ts
+++ src/utils/getUserInfo.ts
@@ -5,14 +5,15 @@ import VConsole from 'vconsole'
 export default async () => {
   const store = useStore()
 
-  const { data: res } = await getUserInfo()
+  if (localStorage.token) {
+    const { data: res } = await getUserInfo()
+    if (res.code == 200) {
+      store.set({ ...res.data, isLogin: true })
+    }
 
-  if (res.code == 200) {
-    store.set({ ...res.data, isLogin: true })
+    if (store.isLogin && store.permissions >= 5) {
+      new VConsole()
+    }
   }
 
-  if (store.isLogin && store.permissions >= 5) {
-    new VConsole()
-
-  }
 }
